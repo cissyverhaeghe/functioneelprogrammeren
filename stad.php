d7ea8e0d8188c6cdf79f23c1e55e8bf0a4d1a3c4
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2021-12-01 12:15:46 +0100

    Exercise completed. Made a config page which can be used to fetch data on the other pages as well. Detail page "stad" was added.

--- stad.php
+++ stad.php
@@ -1,16 +1,20 @@
 <?php
-
 require_once "config.php";
 
 $sql = "select * from images";
+
+
+if ($_GET["img_id"] > "") {
+    $sql .= " where img_id like " . $_GET["img_id"];
+    //var_dump($sql);
+}
 $result = GetData($sql);
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>De leukste plekken in Europa</title>
+    <title>Detail stad</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -29,39 +33,46 @@ $result = GetData($sql);
             crossorigin="anonymous"></script>
 
 </head>
+<style>
+    .information{
+        padding: 25px;
+    }
+    img {
+        display: block;
+        height: auto;
+        width: 75%;
+
+    }
+
+</style>
 <body>
 
 <div class="jumbotron text-center">
-    <h1>De leukste plekken in Europa</h1>
-    <p>Resize this responsive page to see the effect!</p>
+    <h1>Detail Stad</h1>
 </div>
 
-<div class="container">
-    <div class="row">
-        <?php
-        //show result (if there is any)
-        if ($result->num_rows > 0) {
-            //output data of each row
-            while ($row = $result->fetch_assoc()) {
-                echo "<div class='col-sm-4'>
+
+<?php
+//show result (if there is any)
+if ($result->num_rows > 0) {
+    //output data of each row
+    while ($row = $result->fetch_assoc()) {
+        echo " <div class='information'>
             <h3>$row[img_title]</h3>
+            <p>filename: $row[img_filename]</p>
             <p>$row[img_width] x $row[img_height] pixels</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
-            <img src='./images/$row[img_filename]' width='300' height='200'/></br>
-            <a href='stad.php?img_id=$row[img_id]'>Meer info</a>
+            <img src='./images/$row[img_filename]'/></br>
+            <a href='steden2.php'>Terug naar overzicht</a>
             </div>";
 
-                //alle afbeeldingen overlopen, en kolom opmaken met titel en afbeelding
-            }
-        }
-        else
-        {
-            echo "no records found";
-        }
-        //$conn->close();
-        ?>
-    </div>
-</div>
+        //alle afbeeldingen overlopen, en kolom opmaken met titel en afbeelding
+    }
+} else {
+    echo "no records found";
+}
+
+?>
+
+
 </body>
 </html>
