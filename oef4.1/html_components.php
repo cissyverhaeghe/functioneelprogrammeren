3fee6b07195ac51f6a90c78544f1a4ffbb6a62d0
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2021-12-16 10:54:04 +0100

    made exercise 4.1

---
+++ oef4.1/html_components.php
@@ -0,0 +1,27 @@
+<?php
+
+//function to print the head of the page
+function PrintHead()
+{
+    $content = file_get_contents("./templates/head.html");
+    echo $content;
+}
+
+
+//function to print class jumbotron
+function PrintJumbo($header_text,$paragraph_text){
+    $content = file_get_contents("./templates/jumbo.html");
+    $new_content = str_replace( "@@header@@", "$header_text", $content);
+    print str_replace( "@@paragraph@@", "$paragraph_text", $new_content);
+}
+
+//function to print body tag
+function PrintBody(){
+    echo "<body>";
+}
+
+function PrintNavBar()
+{
+    $content = file_get_contents("./templates/navbar.html");
+    echo $content;
+}
---
+++ oef4.1/select.php
@@ -0,0 +1,23 @@
+<?php
+require_once "config.php";
+
+function MakeSelect( $fieldname, $sql, $list_fields = [], $optional = true )
+{
+    $rows = GetData($sql);
+
+    $myselect = "";
+
+    $myselect .= "<select id=$fieldname name=$fieldname>";
+
+    if ( $optional ) $myselect .= "<option></option>";
+
+    foreach ( $rows as $row )
+    {
+        $myselect .= "<option value='" . $row[$list_fields[0]] . "'>" . $row[$list_fields[1]] . "</option>";
+    }
+
+    $myselect .= "</select>";
+
+    print $myselect;
+}
+
