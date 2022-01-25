0c827f9b26f7a94f2e8acf3c218808de5e7df0e7
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-01-25 10:17:38 +0100

    update

--- oef7.1/url_week.php
+++ oef7.1/url_week.php
@@ -17,22 +17,21 @@ PrintNavbar();
 
     PrintTableHead(); // print the head of the table
 
-
-//make a loop to get the days of the week
+    //make a loop to get the days of the week
     for ($i = 17; $i <= 23; $i++) {
-        $data = GetData('SELECT * FROM taak WHERE taa_datum = "2022-01-' . $i . '"');
+
+        $data = GetData('SELECT * FROM taak WHERE taa_datum = "' .$_GET["date"]  . '"');
         $datum = mktime(0, 0, 0, 1, $i, 2022);
         $day = date("d/m/Y", $datum); //get the date in the right format
         $mydate = getdate($datum);
         $weekday = $mydate["weekday"]; //get the weekday from this date
-        if ($data){ //if there is something for this date in the database, print these tasks
+        if ($data) { //if there is something for this date in the database, print these tasks
             $template = file_get_contents("templates/week_row_met_taak.html");
-            $template = str_replace( "@weekday@", $weekday, $template );
-            $template = str_replace( "@day@", $day, $template );
-            $output = MergeViewWithData( $template, $data );
+            $template = str_replace("@weekday@", $weekday, $template);
+            $template = str_replace("@day@", $day, $template);
+            $output = MergeViewWithData($template, $data);
             print $output;
-        }
-        else PrintDayAndDate( $weekday, $day); // else print a row with no tasks
+        } else PrintDayAndDate($weekday, $day); // else print a row with no tasks
     }
 
     PrintTableEnd(); // print the end of the table
