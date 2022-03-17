217f8a121ed0658bb46c7ae30510e7ff5b11c829
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-03-17 09:07:06 +0100

    1.6 done

---
+++ oop1.6/lib/autoload.php
@@ -0,0 +1,65 @@
+<?php
+$app_root = $_SERVER["DOCUMENT_ROOT"] . "/functioneelprogrammeren/oop1.6";
+require_once "$app_root/models/City.php";
+require_once "$app_root/models/User.php";
+require_once "$app_root/services/CityLoader.php";
+require_once "$app_root/services/Container.php";
+require_once "$app_root/services/UserLoader.php";
+require_once "$app_root/services/DBManager.php";
+require_once "$app_root/services/Logger.php";
+require_once "$app_root/services/MessageService.php";
+
+session_start();
+
+
+require_once "connection_data.php";
+require_once "html_functions.php";
+require_once "form_elements.php";
+require_once "sanitize.php";
+require_once "validate.php";
+require_once "security.php";
+require_once "access_control.php";
+
+
+//initialize $errors array
+$errors = [];
+
+if (key_exists('errors', $_SESSION) and is_array($_SESSION['errors'])) {
+    $errors = $_SESSION['errors'];
+    $_SESSION['errors'] = [];
+}
+
+//initialize $msgs array
+$msgs = [];
+
+if (key_exists('msgs', $_SESSION) and is_array($_SESSION['msgs'])) {
+    $msgs = $_SESSION['msgs'];
+    $_SESSION['msgs'] = [];
+}
+
+//initialize $old_post
+$old_post = [];
+
+if (key_exists('OLD_POST', $_SESSION) and is_array($_SESSION['OLD_POST'])) {
+    $old_post = $_SESSION['OLD_POST'];
+    $_SESSION['OLD_POST'] = [];
+}
+
+
+$container = new Container($configuration, $credentials);
+
+$cityLoader = $container->getCityLoader();
+
+
+//create logger
+$logger = $container->getLogger();
+
+
+//create new DBManager
+$dbm = $container->getDBManager();
+
+//create messageService
+
+$ms = $container->getMessageService();
+
+
---
+++ oop1.6/services/Container.php
@@ -0,0 +1,96 @@
+<?php
+
+
+class Container
+{
+    private $configuration;
+
+    private $credentials;
+
+    private $pdo;
+
+    private $cityLoader;
+
+    private $userLoader;
+
+    private $dbManager;
+
+    private $logger;
+
+    private $messageService;
+
+    public function __construct(array $configuration, $credentials)
+    {
+        $this->configuration = $configuration;
+        $this->credentials = $credentials;
+    }
+
+    /**
+     * @return PDO
+     */
+    public function getPDO()
+    {
+        if ($this->pdo === null) {
+            $this->pdo = new PDO($this->configuration['db_dsn'], $this->configuration['db_user'], $this->configuration['db_pass']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return $this->pdo;
+    }
+
+    /**
+     * @return CityLoader
+     */
+    public function getCityLoader()
+    {
+        if ($this->cityLoader === null) {
+            $this->cityLoader = new CityLoader($this->getPDO());
+        }
+        return $this->cityLoader;
+    }
+
+    /**
+     * @return UserLoader
+     */
+    public function getUserLoader()
+    {
+        if ($this->userLoader === null) {
+            $this->userLoader = new UserLoader();
+        }
+        return $this->userLoader;
+    }
+
+    /**
+     * @return DBManager
+     */
+    public function getDBManager()
+    {
+        if ($this->dbManager === null) {
+            $this->dbManager = new DBManager($this->credentials, $this->logger);
+        }
+        return $this->dbManager;
+    }
+
+    /**
+     * @return Logger
+     */
+    public function getLogger()
+    {
+        if ($this->logger === null) {
+            $this->logger = new Logger();
+        }
+        return $this->logger;
+    }
+
+    /**
+     * @return MessageService
+     */
+    public function getMessageService()
+    {
+        if ($this->messageService === null) {
+            $this->messageService = new MessageService();
+        }
+        return $this->messageService;
+    }
+
+}
