ec23bb04d2c15b24d274ea764b4a91e774ae2d88
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-03-22 10:59:13 +0100

    start exercise 12.1

---
+++ oef12.1/steden.php
@@ -0,0 +1,60 @@
+<?php
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+require_once "lib/autoload.php";
+
+PrintHead();
+PrintJumbo( $title = "Leuke plekken in Europa" ,
+                        $subtitle = "Tips voor citytrips voor vrolijke vakantiegangers!" );
+PrintNavbar();
+//PrintMessages();
+
+
+$url_owm = "https://api.openweathermap.org/data/2.5/weather";
+$cissy_owm_key = "99d344fc772c31271136fc8a29770834";
+
+$stad = "Brussel";
+$lang = "nl";
+
+$url = "$url_owm?q=$stad&lang=$lang&appid=$cissy_owm_key";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$curl_response = curl_exec($curl);
+
+print($curl_response);
+
+?>
+
+<div class="container">
+    <div class="row">
+
+<?php
+//    testje foutmelding
+//    $_SESSION["errors"][] = "Geen fouten!";
+
+    $steven = "Steven";
+    $container->getLogger()->Log("DIT BERICHT KOMT UIT STEDEN.PHP");
+    $container->getLogger()->Log( "De variabele steven: " . var_export($steven, true));
+
+
+    //toon messages als er zijn
+    $container->getMessageService()->ShowErrors();
+    $container->getMessageService()->ShowInfos();
+
+    //get data
+    $data = $container->getDBManager()->GetData( "select * from images" );
+
+    //get template
+    $template = file_get_contents("templates/column.html");
+
+    //merge
+    $output = MergeViewWithData( $template, $data );
+    print $output;
+?>
+
+    </div>
+</div>
+
+</body>
+</html>
